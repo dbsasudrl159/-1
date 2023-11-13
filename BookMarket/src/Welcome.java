2c69b423fd78a87fabcfaa648d4680bda335d348
dbsasudrl159 <145322615+dbsasudrl159@users.noreply.github.com>
2023-11-13 16:56:56 +0900

    챕터 7 완성

--- BookMarket/src/Welcome.java
+++ BookMarket/src/Welcome.java
@@ -3,6 +3,8 @@ import java.util.Scanner;
 public class Welcome {
 	static final int NUM_BOOK = 3;
 	static final int NUM_ITEM = 7;
+	static CartItem[] mCartItem = new CartItem[NUM_BOOK];
+	static int mCartCount = 0;
 
 	public static void main(String[] args) {
 		String[][]mBook= new String[NUM_BOOK][NUM_ITEM];
@@ -75,13 +77,24 @@ public class Welcome {
 		System.out.println("7. 영수증 표시하기    \t8. 종료");
 		System.out.println("*****************************************************");
 	}
-	public static void menuGuestInfo(String name, int Mobile){
+	public static void menuGuestInfo(String name, int mobile){
 		System.out.println("\n현재 고객 정보: ");
-		System.out.println("이름: "+name + " "+ "연락처: "+Mobile);
+		//System.out.println("이름: "+name + " "+ "연락처: "+Mobile);
+		Person person = new Person(name, mobile);
+		System.out.println("이름 "+person.getName()+ "  연락처 "+person.getPhone());
 	}
 	
 	public static void menuCartItemList() {
 		System.out.println("\n2. 장바구니 상품 목록 보기: ");
+		System.out.println("---------------------------------------");
+		System.out.println("   도서ID \t|    수량 \t|    합계");
+		for(int i = 0; i < mCartCount; i++) {
+			System.out.print("   " + mCartItem[i].getBookID() + " \t| ");
+			System.out.print("   " + mCartItem[i].getQuantity() + " \t| ");
+			System.out.print("   " + mCartItem[i].getTotalPrice());
+			System.out.print("  ");
+		}
+		System.out.println("---------------------------------------");
 	}
 	
 	public static void menuCartClear() {
@@ -121,12 +134,27 @@ public class Welcome {
 				
 				if(str.toUpperCase().equals("Y")) {
 					System.out.println(book[numId][0]+ "도서가 장바구니에 추가되었습니다.");
+					
+					if(!isCartInBook(book[numId][0])) {
+						mCartItem[mCartCount++] = new CartItem(book[numId]);
+					}
 				}
 				quit =true;
 			}
 			else
 				System.out.println("\n다시 입력해 주세요");
 		}
+	}
+	
+	public static boolean isCartInBook(String bookId) {
+		boolean flag = false;
+		for(int i = 0; i < mCartCount; i++){
+			if(bookId == mCartItem[i].getBookID()) {
+				mCartItem[i].setQuantity(mCartItem[i].getQuantity()+1);
+				flag = true;
+			}
+		}
+		return flag;
 	}
 	
 	public static void menuCartRemoveItemCount() {
